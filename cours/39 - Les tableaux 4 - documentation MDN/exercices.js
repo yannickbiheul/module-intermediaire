93111a29993175bc46dda65ab7e3153ca87af03f
JavaScript de Zéro <jeremy@javascriptdezero.com>
2020-10-23 14:23:16 +0200

    Exercices d'échauffement sur les tableaux

--- cours/39 - Les tableaux 4 - documentation MDN/exercices.js
+++ cours/39 - Les tableaux 4 - documentation MDN/exercices.js
@@ -22,29 +22,122 @@
  */
 
 // Exercice 0
-// Déclarez une fonction merci(prenom, nomSociete) et faites lui retourner
-// une template string qui correspond à la solution de l'énoncé
-
+// Créez la fonction concat0(tableau1, tableau2) ci-dessous.
 
 // Exercice 1
-// Déclarez une fonction formaterAdresse(prenom, nom, adresse, codePostal, ville)
-// et faites lui retourner une template string qui correspond à la solution de l'énoncé
-
+// Créez la fonction concat1(tableau, valeur1, valeur2, valeur3) ci-dessous.
 
 // Exercice 2
-// Déclarez une fonction convertirEnSemaines(nombreDeJours) et faites lui retourner
-// une template string qui correspond à la solution de l'énoncé
-
+// Créez la fonction fill0(tableau, valeur) ci-dessous.
 
 // Exercice 3
-// Déclarez une fonction templateString() et faites lui retourner
-// une template string qui correspond à la solution de l'énoncé
-
+// Créez la fonction fill1(tableau, valeur, index) ci-dessous.
 
 // Exercice 4
-// Déclarez une fonction convertirEnSemainesEtJours(nombreDeJours) et faites lui retourner
-// une template string qui correspond à la solution de l'énoncé.
-// Faites bien attention aux différents pluriels et bon courage !
+// Créez la fonction fill2(tableau, valeur, debut, fin) ci-dessous.
+
+// Exercice 5
+// Créez la fonction fill3(tableau, valeur) ci-dessous.
+
+// Exercice 6
+// Créez la fonction includes0(tableau, valeur) ci-dessous.
+
+// Exercice 7
+// Créez la fonction includes1(tableau, valeur, debut) ci-dessous.
+
+// Exercice 8
+// Créez la fonction includes2(tableau, valeur) ci-dessous.
+
+// Exercice 9
+// Créez la fonction indexOf0(tableau, valeur) ci-dessous.
+
+// Exercice 10
+// Créez la fonction indexOf1(tableau, valeur, debut) ci-dessous.
+
+// Exercice 11
+// Créez la fonction indexOf2(tableau, valeur) ci-dessous.
+
+// Exercice 12
+// Créez la fonction join0(tableau) ci-dessous.
+
+// Exercice 13
+// Créez la fonction join1(tableau, separateur) ci-dessous.
+
+// Exercice 14
+// Créez la fonction lastIndexOf0(tableau, valeur) ci-dessous.
+
+// Exercice 15
+// Créez la fonction lastIndexOf1(tableau, valeur) ci-dessous.
+
+// Exercice 16
+// Créez la fonction lastIndexOf2(tableau, valeur) ci-dessous.
+
+// Exercice 17
+// Créez la fonction pop0(tableau) ci-dessous.
+
+// Exercice 18
+// Créez la fonction pop1(tableau) ci-dessous.
+
+// Exercice 19
+// Créez la fonction push0(tableau, valeur) ci-dessous.
+
+// Exercice 20
+// Créez la fonction push1(tableau, valeur1, valeur2) ci-dessous.
+
+// Exercice 21
+// Créez la fonction reverse0(tableau) ci-dessous.
+
+// Exercice 22
+// Créez la fonction shift0(tableau) ci-dessous.
+
+// Exercice 23
+// Créez la fonction shift1(tableau) ci-dessous.
+
+// Exercice 24
+// Créez la fonction slice0(tableau) ci-dessous.
+
+// Exercice 25
+// Créez la fonction slice1(tableau, debut, fin) ci-dessous.
+
+// Exercice 26
+// Créez la fonction slice2(tableau) ci-dessous.
+
+// Exercice 27
+// Créez la fonction slice3(tableau) ci-dessous.
+
+// Exercice 28
+// Créez la fonction splice0(tableau) ci-dessous.
+
+// Exercice 29
+// Créez la fonction splice1(tableau) ci-dessous.
+
+// Exercice 30
+// Créez la fonction splice2(tableau) ci-dessous.
+
+// Exercice 31
+// Créez la fonction splice3(tableau, debut, valeur) ci-dessous.
+
+// Exercice 32
+// Créez la fonction splice4(tableau, debut, valeur1, valeur2) ci-dessous.
+
+// Exercice 33
+// Créez la fonction splice5(tableau, valeur1, valeur2) ci-dessous.
+
+// Exercice 34
+// Créez la fonction splice6(tableau, valeur1, valeur2) ci-dessous.
+
+// Exercice 35
+// Créez la fonction unshift0(tableau, valeur) ci-dessous.
+
+// Exercice 36
+// Créez la fonction unshift1(tableau, valeur1, valeur2) ci-dessous.
+
+/********************/
+/* Exercices finaux */
+/********************/
+
+// Exercice 0 — Entrelacement
+// Créez la fonction entrelacement(tableau1, tableau2) ci-dessous.
 
 
 // Exercice 5
--- cours/39 - Les tableaux 4 - documentation MDN/solutions.js
+++ cours/39 - Les tableaux 4 - documentation MDN/solutions.js
@@ -5,63 +5,290 @@
  */
 
 // Exercice 0
-function merci(prenom, nomSociete) {
-  return `C'est ${prenom} qui m'a permis d'entrer dans ${nomSociete}`;
+function concat0(tableau1, tableau2) {
+  return tableau1.concat(tableau2);
 }
 
 // Exercice 1
-function formaterAdresse(prenom, nom, adresse, codePostal, ville) {
-  return `===[ Destinataire ]===
-${prenom} ${nom}
-${adresse}
-${codePostal} ${ville}`;
+function concat1(tableau, valeur1, valeur2, valeur3) {
+  return tableau.concat(valeur1, valeur2, valeur3);
 }
 
 // Exercice 2
-function convertirEnSemaines(nombreDeJours) {
-  return `${nombreDeJours} jours sont équivalents à ${nombreDeJours / 7} semaines`;
+function fill0(tableau, valeur) {
+  return tableau.fill(valeur);
 }
 
 // Exercice 3
-function templateString() {
-  return `Les template strings sont délimitées par des backticks \`\`.
-Pour créer un espace réservé on utilise \${}.`;
+function fill1(tableau, valeur, index) {
+  return tableau.fill(valeur, index);
 }
 
 // Exercice 4
-function convertirEnSemainesEtJours(nombreDeJours) {
-  let nombreDeSemaines = 0;
-  let nombreDeJoursRestants = nombreDeJours;
-
-  while(nombreDeJoursRestants >= 7) {
-    nombreDeJoursRestants = nombreDeJoursRestants - 7;
-    nombreDeSemaines = nombreDeSemaines + 1;
-  }
-
-  // Gestion des singuliers et pluriels
-  let plurielNombreDeJours = "";
-  if (nombreDeJours > 1) {
-    plurielNombreDeJours = "s";
-  }
-
-  let plurielJoursRestants = "";
-  if (nombreDeJoursRestants > 1) {
-    plurielJoursRestants = "s";
-  }
-
-  let plurielNombreDeSemaines = "";
-  if (nombreDeSemaines > 1) {
-    plurielNombreDeSemaines = "s";
-  }
-
-  return `${nombreDeJours} jour${plurielNombreDeJours}, c'est équivalent à ${nombreDeSemaines} semaine${plurielNombreDeSemaines} et ${nombreDeJoursRestants} jour${plurielJoursRestants}`;
+function fill2(tableau, valeur, debut, fin) {
+  return tableau.fill(valeur, debut, fin + 1);
 }
 
 // Exercice 5
-function retournerReference(numeroClient) {
-  let typeCompte = "standard";
-  if (numeroClient >= 5000 && numeroClient < 7000) {
-    typeCompte = "premium";
-  }
-  return `N° de client : ${formaterNumeroClient(numeroClient)} (${typeCompte})`;
+function fill3(tableau, valeur) {
+  return tableau.fill(valeur, -3);
 }
+
+// Exercice 6
+function includes0(tableau, valeur) {
+  return tableau.includes(valeur);
+}
+
+// Exercice 7
+function includes1(tableau, valeur, debut) {
+  return tableau.includes(valeur, debut);
+}
+
+// Exercice 8
+function includes2(tableau, valeur) {
+  return tableau.includes(valeur, -4);
+}
+
+// Exercice 9
+function indexOf0(tableau, valeur) {
+  return tableau.indexOf(valeur);
+}
+
+// Exercice 10
+function indexOf1(tableau, valeur, debut) {
+  return tableau.indexOf(valeur, debut);
+}
+
+// Exercice 11
+function indexOf2(tableau, valeur) {
+  return tableau.indexOf(valeur, -2);
+}
+
+// Exercice 12
+function join0(tableau) {
+  return tableau.join();
+}
+
+// Exercice 13
+function join1(tableau, separateur) {
+  return tableau.join(separateur);
+}
+
+// Exercice 14
+function lastIndexOf0(tableau, valeur) {
+  return tableau.lastIndexOf(valeur);
+}
+
+// Exercice 15
+function lastIndexOf1(tableau, valeur) {
+  return tableau.lastIndexOf(valeur, 3);
+}
+
+// Exercice 16
+function lastIndexOf2(tableau, valeur) {
+  return tableau.lastIndexOf(valeur, -3);
+}
+
+// Exercice 17
+function pop0(tableau) {
+  return tableau.pop();
+}
+
+// Exercice 18
+function pop1(tableau) {
+  tableau.pop();
+  return tableau;
+}
+
+// Exercice 19
+function push0(tableau, valeur) {
+  tableau.push(valeur);
+  return tableau;
+}
+
+// Exercice 20
+function push1(tableau, valeur1, valeur2) {
+  tableau.push(valeur1, valeur2);
+  return tableau;
+}
+
+// Exercice 21
+function reverse0(tableau) {
+  return tableau.reverse();
+}
+
+// Exercice 22
+function shift0(tableau) {
+  return tableau.shift();
+}
+
+// Exercice 23
+function shift1(tableau) {
+  tableau.shift();
+  return tableau;
+}
+
+// Exercice 24
+function slice0(tableau) {
+  return tableau.slice(2);
+}
+
+// Exercice 25
+function slice1(tableau, debut, fin) {
+  return tableau.slice(debut, fin);
+}
+
+// Exercice 26
+function slice2(tableau) {
+  return tableau.slice(-3);
+}
+
+// Exercice 27
+function slice3(tableau) {
+  return tableau.slice(2,-2);
+}
+
+// Exercice 28
+function splice0(tableau) {
+  tableau.splice(2,1);
+  return tableau;
+}
+
+// Exercice 29
+function splice1(tableau) {
+  tableau.splice(1,3);
+  return tableau;
+}
+
+// Exercice 30
+function splice2(tableau) {
+  tableau.splice(-3,2);
+  return tableau;
+}
+
+// Exercice 31
+function splice3(tableau, debut, valeur) {
+  tableau.splice(debut, 0, valeur);
+  return tableau;
+}
+
+// Exercice 32
+function splice4(tableau, debut, valeur1, valeur2) {
+  tableau.splice(debut, 0, valeur1, valeur2);
+  return tableau;
+}
+
+// Exercice 33
+function splice5(tableau, valeur1, valeur2) {
+  tableau.splice(-2, 0, valeur1, valeur2);
+  return tableau;
+}
+
+// Exercice 34
+function splice6(tableau, valeur1, valeur2) {
+  tableau.splice(1, 3, valeur1, valeur2);
+  return tableau;
+}
+
+// Exercice 35
+function unshift0(tableau, valeur) {
+  tableau.unshift(valeur);
+  return tableau;
+}
+
+// Exercice 36
+function unshift1(tableau, valeur1, valeur2) {
+  tableau.unshift(valeur1, valeur2);
+  return tableau;
+}
+
+/********************/
+/* Exercices finaux */
+/********************/
+
+// Exercice 0 — Entrelacement
+function entrelacement(tableau1, tableau2) {
+  const resultat = [];
+  for (let i = 0; i < tableau1.length; i++) {
+    resultat.push(tableau1[i], tableau2[i]);
+  }
+  return resultat;
+}
+
+// Exercice 1
+
+
+// Exercice 2
+
+
+// Exercice 3
+
+
+// Exercice 4
+
+
+// Exercice 5
+
+
+// Exercice 6
+
+
+// Exercice 7
+
+
+// Exercice 8
+
+
+// Exercice 9
+
+
+// Exercice 10
+
+
+// Exercice 11
+
+
+// Exercice 12
+
+
+// Exercice 13
+
+
+// Exercice 14
+
+
+// Exercice 15
+
+
+// Exercice 16
+
+
+// Exercice 17
+
+
+// Exercice 18
+
+
+// Exercice 19
+
+
+// Exercice 20
+
+
+// Exercice 21
+
+
+// Exercice 22
+
+
+// Exercice 23
+
+
+// Exercice 24
+
+
+// Exercice 25
+
+
+// Exercice 26
+
